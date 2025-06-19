f4554a0df883ff0c70c45dfc0ffc3a6f3fa97ad8
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2025-06-19 18:23:33 -0300

    feat: adiciona cenários de testes

---
+++ src/main/java/rest/tests/BarrigaTest.java
@@ -0,0 +1,103 @@
+package rest.tests;
+
+import core.BaseTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class BarrigaTest extends BaseTest {
+
+    private String token;
+
+    @Before
+    public void login() {
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "wagner@aquino");
+        login.put("senha", "123456");
+
+        token = given()
+                .body(login)
+                .when()
+                .post("/signin")
+                .then()
+                .statusCode(200)
+                .extract().path("token");
+    }
+
+    @Test
+    public void naoDeveAcessarSemToken() {
+        given()
+                .when()
+                    .get("/contas")
+                .then()
+                    .statusCode(401);
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso(){
+                given()
+                        .header("Authorization", "JWT " + token)
+                        .body("{\"nome\": \"Conta para teste restAssured\"}")
+                .when()
+                    .post("/contas")
+                .then()
+                        .statusCode(201)
+                ;
+    }
+
+    @Test
+    public void alterarContaComSucesso() {
+        given()
+                    .header("Authorization", "JWT " + token)
+                    .body("{\"nome\": \"Conta alterada\"}")
+                .when()
+                    .put("/contas/2492738")
+                .then()
+                    .statusCode(200)
+                .log().all()
+                .body("nome", is("Conta alterada"))
+        ;
+    }
+
+    @Test
+    public void naoDeveIncluirContaComMesmoNome(){
+        given()
+                .header("Authorization", "JWT " + token)
+                .body("{\"nome\": \"Conta alterada\"}")
+            .when()
+                .post("/contas")
+            .then()
+                .statusCode(400)
+                .body("error", is("Já existe uma conta com esse nome!"))
+        ;
+    }
+
+    @Test
+    public void deveInserirMovimentacaoComSucesso(){
+        Movimentacao movimentacao = new Movimentacao();
+        movimentacao.setConta_id(2492738);
+//        mov.setUsuario_id();
+        movimentacao.setDescricao("Teste descrição da movimentação");
+        movimentacao.setEnvolvido("Envolvido na mov");
+        movimentacao.setTipo("REC");
+        movimentacao.setData_transacao("01/01/2025");
+        movimentacao.setData_pagamento("10/06/2025");
+        movimentacao.setStatus(true);
+
+        given()
+                .header("Authorization", "JWT " + token)
+                .body(movimentacao)
+            .when()
+                .post("/transacoes")
+            .then()
+                .statusCode(201)
+                ;
+    }
+
+}
